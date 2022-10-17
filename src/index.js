9bced5b775f904f3818eaf2b0286cd5056475fbc
Patrick Mason <patrickmason73@gmail.com>
2022-10-17 10:15:21 -0600

    task elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault(event)
 });
+
+let createTaskForm = document.getElementById("create-task-form")
+
+createTaskForm.addEventListener('click', submitTask)
+
+function submitTask(event) {
+  let ul = document.getElementById("tasks")
+  let li = document.createElement("li")
+  li.textContent = `${event.target }`
+}
